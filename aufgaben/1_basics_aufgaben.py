93a72c572dfab24c6dc3219880d0156f3c9b9347
Felix Reinbott <Felix.Ka@web.de>
2021-04-27 15:10:13 +0200

    finishing first draft of chapter 1 and some ideas for exercises

---
+++ aufgaben/1_basics_aufgaben.py
@@ -0,0 +1,19 @@
+"""
+TODO
+"""
+
+# Aufgabe 1: 
+# 
+# a)
+# Schreiben sie eine Funktion "add_values" die die Parameter x1 und x2 erhält. 
+# Die Funktion soll "x1 + x2" zurückgeben. 
+#
+# b) 
+# Testen sie die oben erstellte funktion mit den Variablen 
+x1, x2 = 3, 4
+y1, y2 = "Hallo", "RTG"
+# Was fällt ihnen dabei auf ? (Denken sie dabei an die Datentypen der Variblen)
+# Schreiben sie ihre Beobacktungen als Kommentar in dieses File.
+
+# Aufgabe 2:
+
