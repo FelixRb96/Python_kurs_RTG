1c31a67d8b830f601ff054c55b7341b800289d82
Till-1 <55398540+Till-1@users.noreply.github.com>
2021-04-28 17:43:47 +0200

    Update 4_pandas_aufgaben.py

---
+++ aufgaben/4_pandas_aufgaben.py
@@ -0,0 +1,67 @@
+# Importiert zunächst pandas und numpy
+
+marvel_data = [
+    ['Spider-Man', 'male', 1962],
+    ['Captain America', 'male', 1941],
+    ['Wolverine', 'male', 1974],
+    ['Iron Man', 'male', 1963],
+    ['Thor', 'male', 1963],
+    ['Thing', 'male', 1961],
+    ['Mister Fantastic', 'male', 1961],
+    ['Hulk', 'male', 1962],
+    ['Beast', 'male', 1963],
+    ['Invisible Woman', 'female', 1961],
+    ['Storm', 'female', 1975],
+    ['Namor', 'male', 1939],
+    ['Hawkeye', 'male', 1964],
+    ['Daredevil', 'male', 1964],
+    ['Doctor Strange', 'male', 1963],
+    ['Hank Pym', 'male', 1962],
+    ['Scarlet Witch', 'female', 1964],
+    ['Wasp', 'female', 1963],
+    ['Black Widow', 'female', 1964],
+    ['Vision', 'male', 1968]
+]
+
+#Erstellt einen Dataframe aus marvel_data
+
+
+#Bennent die Spalten durch die gegebene Liste 
+col_names = ['name', 'sex', 'first_appearance']
+
+
+#Macht die Spalte Index zu dem Index
+
+
+#dropt die entsprechende Spalte name
+
+
+#droppt die Zeilen mit Namor und Hank Pym
+
+
+#zeigt die ersten 5 Zeilen des DataFrames an
+
+
+
+#Zeig die erste und letzte Zeile 
+
+
+
+#Füge eine Neue Spalte mit der Anzahl der Jahre seit der Veröffentlichung hinzu
+
+
+
+# Ändert die Ersterscheinung von Vision auf 1964
+
+
+#Lasst euch alle weiblichen Superheldinnen ausgeben 
+
+
+
+#Lasst euch den Durschnitt der Ersterscheinungen ausgeben 
+
+
+#Plottet die ersten experiences
+
+
+#Lasst euch ein Histogramm der Ersterscheinungen ausgeben
