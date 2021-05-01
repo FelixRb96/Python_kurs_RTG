b95e9a9b9a170067fd8619401cd7f2819e108612
Felix Reinbott <Felix.Ka@web.de>
2021-05-01 18:58:42 +0200

    finishing additional exercises and commenting for last part

---
+++ aufgaben/5_weiterfuehrende_aufgaben.py
@@ -0,0 +1,17 @@
+"""
+
+"""
+
+# Aufgabe 1
+# a) Laden sie sich den Datensatz "digits" aus dem Paket scikit-learn
+# b) machen sie sich mit den Hyperparametern eines Random Forrest aus dem modul ensemble vertraut
+# c) Grid search: Teste ein Gitter an Hyperparametern ab, und wähle zum Schluss die beste Kombination
+# aus Parametern. Schreiben sie eine Funktion, die Grid search für einen Random Forrest auf dem Datensatz
+# digits ausführt. Speichern sie sich die besten Hyperparameter ab.
+
+# Aufgabe 2
+# a) Schreiben sie den Code aus dem numpy notebook so um, dass sie sich Pfade einer Mehrdimensionalen 
+# Brownschen Bewegung simulieren können. Plotten sie sich den Pfad einer zweidimensionalen 
+# Brownschen Bewegung den sie mit ihrer Lösung gesamplet haben.
+# c) Samplen sie einen Pfad einer dreidimensionalen Brownschen Bewegung und finden sie eine 
+# entsprechende Funktion oder eine entesprechende Library um diese als 3d plot zu visualisieren.
